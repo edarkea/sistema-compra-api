e230d0c39f5f473d477ed00e6ef928613fdce89d
Edinson Ayui <edinson.ayui@gmail.com>
2023-01-21 02:04:58 -0500

    se agregaron los paths que impiden que swagger se despliegue

--- src/main/java/org/edarkea/compra/api/CompraApplication.java
+++ src/main/java/org/edarkea/compra/api/CompraApplication.java
@@ -3,15 +3,12 @@ package org.edarkea.compra.api;
 import org.edarkea.compra.api.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CompraApplication {
@@ -19,7 +16,7 @@ public class CompraApplication {
     public static void main(String[] args) {
         SpringApplication.run(CompraApplication.class, args);
     }
-    
+
     @EnableWebSecurity
     @Configuration
     class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,6 +28,12 @@ public class CompraApplication {
                     .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                     .authorizeRequests()
                     .antMatchers(HttpMethod.GET, "/api/test/all").permitAll()
+                    .antMatchers(HttpMethod.GET,
+                            "/v2/api-docs",
+                            "/configuration/ui",
+                            "/swagger-resources/**",
+                            "/swagger-ui.html",
+                            "/webjars/**").permitAll()
                     .anyRequest()
                     .authenticated();
         }
